c19a151390a26cfc895cd51bc894914fa98b84af
Matt Cowgill <mattcowgill@gmail.com>
2020-01-11 21:31:08 +1100

    fix internal data

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -2,6 +2,8 @@ library(grid)
 library(png)
 library(usethis)
 
+# Logo and footer -----
+
 png_to_grob <- function(path) {
 
   image <- png::readPNG(source = path)
@@ -14,8 +16,17 @@ png_to_grob <- function(path) {
 footer_left <- png_to_grob("data-raw/footer_left.png")
 footer_right <- png_to_grob("data-raw/footer_right.png")
 
+# Colours -----
+
+arc_green <- "#f0f7ee"
+
+arc_dark_grey <- "#333333"
+
+# Save data -----
 
 usethis::use_data(footer_left,
                   footer_right,
+                  arc_green,
+                  arc_dark_grey,
                   internal = TRUE,
                   overwrite = TRUE)
