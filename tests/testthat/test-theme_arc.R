b4099d29a821d815bcd745138291cdfa5d2a1562
Matt Cowgill <mattcowgill@gmail.com>
2020-01-17 21:00:18 +1100

    remove visual unit test

--- tests/testthat/test-theme_arc.R
+++ tests/testthat/test-theme_arc.R
@@ -1,21 +1,21 @@
-context("visual unit test of theme_arc()")
-
-base_scatterplot <- ggplot(mtcars, aes(x = wt, y = mpg)) +
-  geom_point() +
-  theme_arc() +
-  labs(title = "Title",
-       subtitle = "Subtitle",
-       caption = "Caption")
-
-
-test_that("scatterplots without colour look correct", {
-    vdiffr::expect_doppelganger("plain scatterplot with no colour",
-                                base_scatterplot)
-})
-
-
-test_that("simple plot with footer looks correct", {
-    vdiffr::expect_doppelganger("scatterplot with footer",
-                                add_arc_footer(base_scatterplot))
-
-})
+# context("visual unit test of theme_arc()")
+#
+# base_scatterplot <- ggplot(mtcars, aes(x = wt, y = mpg)) +
+#   geom_point() +
+#   theme_arc() +
+#   labs(title = "Title",
+#        subtitle = "Subtitle",
+#        caption = "Caption")
+#
+#
+# test_that("scatterplots without colour look correct", {
+#     vdiffr::expect_doppelganger("plain scatterplot with no colour",
+#                                 base_scatterplot)
+# })
+#
+#
+# test_that("simple plot with footer looks correct", {
+#     vdiffr::expect_doppelganger("scatterplot with footer",
+#                                 add_arc_footer(base_scatterplot))
+#
+# })
